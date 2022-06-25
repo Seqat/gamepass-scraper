057a816bb28e189cf2936b1a2aa52d1215ac480d
Sedat Ali Zevit <37043095+Seqat@users.noreply.github.com>
2022-06-25 16:21:51 +0000

    Add .gitignore and methods for creating csv file

--- scraper.py
+++ scraper.py
@@ -1,23 +1,60 @@
 from requests_html import HTMLSession
+import csv
 
+def GamePass_Scraper(url,xpath):
+    """_summary_: Scrapes the gamepass website for the games
 
-def GamePass_Scraper():
+    Args:
+        url (_type_): URL of the website
+        xpath (_type_): XPath of the list of games
+    """
     session = HTMLSession()
 
-    response = session.get(
-        'https://gamepasscounter.com/')
-    
-    # Take the response and parse it into a HTML object
-    games = response.html.xpath('//*[@id="row2"]/div[2]/div[3]/div/ul')
-    
-    # Split the games into a list
-    games=games[0].text.split('\n')
+    response = session.get(url
+        )
 
+    # Take the response and parse it into a HTML object
+    games = response.html.xpath(xpath)
+
+    # Split the games into a list
+    games = games[0].text.split('\n')
+
+    # prints the games
+    game_printer(games)
     
+    # writes the games to a csv file
+    csv_writer(games)
+
+
+def game_printer(games):
+    """Summary: Prints the number of games in the list, and the games themselves
+
+    Args:
+        games (String): The list of games in string format
+    """
     print(f"{len(games)} games found ! \n")
-    print(games)
-    
+
+    for game in games:
+        print(game)
+
+
+def csv_writer(games):
+    """Summary: Writes the games to a csv file
+
+    Args:
+        games (String): The list of games in string format
+    """
+    # Create a csv file and write the games to it
+    with open('games.csv', 'w', encoding="UTF-8") as csv_file:
+        writer = csv.writer(csv_file)
+        
+        for game in games:
+            writer.writerow([game])
+            
+        
+
 
 # if script is run directly, run the scraper
 if __name__ == '__main__':
-    GamePass_Scraper()
+    GamePass_Scraper(   'https://gamepasscounter.com/',
+                        '//*[@id="row2"]/div[2]/div[3]/div/ul')
